b49a346bd47bf393c2d0739e652dac5c5fce00ee
Lu <gelululuyuan@gmail.com>
2025-07-14 16:27:00 +0800

    Starting to let LLM visual analyze the headers

--- agents/test.py
+++ agents/test.py
@@ -1,11 +1,12 @@
+#测试图片识别api
+from langchain_openai import ChatOpenAI
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from utilities.modelRelated import invoke_model
+import base64
 
-response = invoke_model(model_name="gpt-4o", messages=[
-    SystemMessage(content="You are a helpful assistant."),
-    HumanMessage(content="What is the capital of France?")
-])
-print(response)
+
+
+
 
 
 
